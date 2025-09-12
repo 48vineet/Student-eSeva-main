8ab50299cace22820bb2e1a9695890ef89b447cf
Vineet Mali <7038vineet@gmail.com>
2025-09-12 15:38:10 +0530

    commit

--- backend/config/env.js
+++ backend/config/env.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  port: process.env.PORT || 5000,
+  port: process.env.PORT || 5001,
   mongoUri: process.env.MONGODB_URI,
   jwtSecret: process.env.JWT_SECRET,
   mlServiceUrl: process.env.ML_SERVICE_URL,
