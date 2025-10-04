cbd7cf02ff7c6ee3441ebbfc7be6cdc93c74afc5
Vineet Mali <7038vineet@gmail.com>
2025-10-04 11:03:05 -0700

    Update ConfigContext.jsx

--- frontend/src/context/ConfigContext.jsx
+++ frontend/src/context/ConfigContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { api } from "../api/api"; 
-
+ 
 const ConfigContext = createContext();
 
 export function ConfigProvider({ children }) {
